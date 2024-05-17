540dadc4d6bcad7c8533decec93e262975b15763
pooya <mras.arab@gmail.com>
2024-05-17 12:09:31 +0200

    first commit

---
+++ src/main/java/com/example/demo/entity/Student.java
@@ -0,0 +1,41 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "student")
+public class Student {
+
+
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    private Long studentId;
+    private String firstName;
+    private String lastName;
+
+    @Column(
+            name = "email",
+            unique = true,
+            nullable = false
+    )
+    private String emailId;
+
+
+
+}
